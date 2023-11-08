6af978bc27b1c8b21c59e1f10f50bd878ce7bb0a
Ahmed Baari <ahmed4baari@gmail.com>
2023-11-08 04:41:31 +0000

    completed DFT in Graph

--- labcia2/graph-self.cpp
+++ labcia2/graph-self.cpp
@@ -84,7 +84,7 @@ public:
         return true;
     }
 
-    gvertex *pop()
+    gvertex *pop() // * for pop
     {
         node *victim = top;
         gvertex *poppedData = victim->vertex;
@@ -92,6 +92,11 @@ public:
         delete victim;
         count--;
         return poppedData;
+    }
+
+    node *topstack()
+    {
+        return top;
     }
 };
 
@@ -188,15 +193,15 @@ public:
 
 
         /* Inserting ARC in FEMALE to target MALE */
-        garc *greenie = new garc();
+        garc *greenie2 = new garc();
 
-        greenie->content = male;
-        greenie->nextArc = NULL;
+        greenie2->content = male;
+        greenie2->nextArc = NULL;
 
-        // Inserting MALE arc in MALE Relationships
+        // Inserting FEMALE arc in FEMALE Relationships
         if (female->firstArc == NULL)
         {
-            female->firstArc = greenie;
+            female->firstArc = greenie2;
         }
         else
         {
@@ -208,7 +213,7 @@ public:
             {
                 runner = runner->nextArc;
             }
-            runner->nextArc = greenie;
+            runner->nextArc = greenie2;
         }
     }
 
@@ -217,19 +222,68 @@ public:
         Stack DFTstack;
 
         // We will keep count of visited vertexes, to compare with the vertexcount
-        int visitedVertexes = 0;
+        int visitedVertices = 0;
 
-        // Create the runner
+        // The Vertex Runner for DFT
         gvertex *runner = firstVertex;
 
         // Push the first vertex to begin
         DFTstack.push(runner);
         runner->processed = true;
         runner->inStack = true;
-        visitedVertexes++;
+        visitedVertices++;
         cout << runner->data << "\t"; //print data
 
-        // Get Stack Top
+        // Now perform the traversal as long as all vertexes are visited
+        while (count != visitedVertices) {
+            // Get the stack top 
+            /*
+            Vertex & next arc
+            */
+            node *stackTop = DFTstack.topstack();
+
+            // Start a runner at the vertex's relationships list
+            garc *ARCrunner = stackTop->vertex->firstArc;
+
+            // forward to the relationship which is NOT PROCESSED
+            while (ARCrunner != NULL && ARCrunner->content->processed == true) {
+                ARCrunner = ARCrunner->nextArc;
+            }
+            /* If we have reached an unprocessed arc, do the rituals */
+            if (ARCrunner != NULL) {
+                gvertex *targetVertex = ARCrunner->content;
+                DFTstack.push(targetVertex); // push the content to stack
+                targetVertex->processed = true;
+                targetVertex->inStack = true;
+                visitedVertices++;
+                cout << targetVertex->data << "\t";
+            }
+
+            else {
+                /* This is done when the vertex at top of stack doesn't have any adjascent unprocessed vertexes */
+                gvertex *poppingVertex = DFTstack.pop();
+                poppingVertex->inStack = false;
+
+                /* If the stack is empty, then that network is over. We'll try again with the next UNPROCESSED vertex */
+                if (DFTstack.topstack() == NULL) {
+                    runner = firstVertex->nextVertex; // Just making it more efficient by starting in 2nd instead of 1st vertex
+
+                    while(runner != NULL && runner->processed == true) {
+                        runner = runner->nextVertex;
+                    }
+
+                    /* If we find an unprocessed vertex */
+                    if (runner !=NULL) {
+                        DFTstack.push(runner);
+                        runner->processed = true;
+                        runner->inStack = true;
+                        visitedVertices++;
+                        cout << runner->data << "\t";
+                    }
+                }
+            }
+        }
+        cout << "\n";
 
         // Traverse to next unprocessed arc
         
@@ -239,5 +293,50 @@ public:
 
 int main()
 {
-    cout << "Hello World" << endl;
+    graph myGraph;
+    int choice;
+    char vertexData, startVertexData, endVertexData;
+
+    do
+    {
+        cout << "\n=============================\n";
+        cout << "           MENU\n";
+        cout << "=============================\n";
+        cout << "1. Insert Vertex\n";
+        cout << "2. Insert Edge\n";
+        cout << "3. Depth First Search\n";
+        cout << "0. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                cout << "Enter character to insert: ";
+                cin >> vertexData;
+                myGraph.insertVertex(vertexData);
+                break;
+
+            case 2:
+                cout << "Enter characters to add edge: ";
+                cin >> startVertexData >> endVertexData;
+                if (myGraph.insertArc(startVertexData, endVertexData))
+                    cout << "Edge inserted between " << startVertexData << " and " << endVertexData << "." << endl;
+                else
+                    cout << "Either one or both vertices not present in graph.\n";
+                break;
+
+            case 3:
+                myGraph.depthFirstTraversal();
+                break;
+
+            case 0:
+                break;
+
+            default:
+                cout << "Invalid choice!\n";
+        }
+    } while (choice != 0);
+
+    return 0;
 }
